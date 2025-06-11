8931eda643eb3448d8ebcb101a0f7431608397fc
Supratimdas20 <supratimdaswork@gmail.com>
2025-06-11 20:43:51 +0530

    Day 5: Working with 2 parameter in a function in OOPs.

--- LearnJavaPractice/src/main/java/org/example/Footwear/footwearProgram.java
+++ LearnJavaPractice/src/main/java/org/example/Footwear/footwearProgram.java
@@ -3,5 +3,6 @@ package org.example.Footwear;
 public class footwearProgram {
     public static void main(String[] args){
         System.out.println("Hello!");
+        System.out.println("Hello World!");
     }
 }

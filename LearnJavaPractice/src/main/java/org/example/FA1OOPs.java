f863557cd412a481f4ae050a1d0e0fb4f610ed74
Supratimdas20 <supratimdaswork@gmail.com>
2025-06-15 09:43:11 +0530

    Day 8: Working with 2 parameter in a function in OOP.

--- LearnJavaPractice/src/main/java/org/example/FA1OOPs.java
+++ LearnJavaPractice/src/main/java/org/example/FA1OOPs.java
@@ -1,39 +1,67 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.Scanner; // Importing Scanner class for user input
+import java.util.*; // Importing all classes from java.util package (redundant here)
 
 public class FA1OOPs {
     public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        int n= sc.nextInt();sc.nextLine();
-        Truck[] truck = new Truck[n];
+        Scanner sc= new Scanner(System.in); // Creating a Scanner object for input
+
+        int n= sc.nextInt();sc.nextLine(); // Reading the number of trucks and consuming the newline character
+        Truck[] truck = new Truck[n]; // Creating an array to store Truck objects
+
+        // Loop to take input for each Truck object
         for(int i=0;i<n;i++){
-            int truckId=sc.nextInt();sc.nextLine();
-            String manufacture=sc.nextLine();
-            double capacity =sc.nextDouble();sc.nextLine();
-            double mileage = sc.nextDouble();
-            truck[i] =new Truck(truckId,manufacture,capacity,mileage);
+            int truckId=sc.nextInt();sc.nextLine(); // Reading truckId and consuming newline
+            String manufacture=sc.nextLine(); // Reading manufacturer name
+            double capacity =sc.nextDouble();sc.nextLine(); // Reading truck capacity and consuming newline
+            double mileage = sc.nextDouble(); // Reading mileage
+            truck[i] =new Truck(truckId,manufacture,capacity,mileage); // Creating Truck object and storing it in array
         }
-        findTruckWithMinimumCapacity(truck);
+
+        String companyName=sc.nextLine(); // Reading the name of the manufacturer to search for
+        findTruckWithMinimumCapacity(truck,companyName); // Calling method to find truck with minimum capacity
     }
-    public static void findTruckWithMinimumCapacity(Truck[] truck){
-        double min = Double.MAX_VALUE;
-        int truckWithMinCap= Integer.parseInt(null);
-        for(int i=0;i<truck.length;i++){
-            if(min>truck[i].capacity){
-                min=truck[i].capacity;
-                truckWithMinCap=i;
+
+    // Method to find and display the truck with the minimum capacity for a given manufacturer
+    public static void findTruckWithMinimumCapacity(Truck[] truck,String str){
+        double min = Double.MAX_VALUE; // Initializing minimum capacity to the highest possible value
+        int truckWithMinCap= -1; // Variable to store index of truck with minimum capacity
+
+        // Looping through the truck array
+        for(int i=0;i<truck.length;i++) {
+            // Checking if the truck's manufacturer matches the input string
+            if (truck[i].manufacturer.trim().equalsIgnoreCase(str.trim()))
+            {
+                // Updating the minimum capacity if a smaller one is found
+                if (min > truck[i].capacity) {
+                    min = truck[i].capacity;
+                    truckWithMinCap = i; // Storing the index of the truck with minimum capacity
+                }
             }
-//            [1,8,7,6,5,] min>
+        }
+
+        // If no matching truck is found, display "No Truck Found"
+        if(truckWithMinCap ==-1){
+            System.out.println("No Truck Found");
+        } else {
+            // Displaying the details of the truck with minimum capacity
+            System.out.println("truckId-" + truck[truckWithMinCap].truckId);
+            System.out.println("manufacturer-" + truck[truckWithMinCap].manufacturer);
+            System.out.println("capacity-" + truck[truckWithMinCap].capacity);
+            System.out.println("mileage-" + truck[truckWithMinCap].mileage);
         }
     }
 }
-class Truck{
-    public int truckId;
-    public String manufacturer;
-    public double capacity;
-    public double mileage;
 
+// Truck class to represent a truck object
+class Truck{
+    public int truckId; // Truck ID
+    public String manufacturer; // Manufacturer name
+    public double capacity; // Capacity of the truck
+    public double mileage; // Mileage of the truck
+
+    // Constructor to initialize truck attributes
     public Truck(int truckId,String manufacture,double capacity,double mileage){
         this.truckId=truckId;
         this.manufacturer=manufacture;

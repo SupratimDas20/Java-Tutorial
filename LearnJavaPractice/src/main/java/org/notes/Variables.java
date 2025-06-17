deeb2845724101481eaff1c341afc31520c172e7
Supratimdas20 <supratimdaswork@gmail.com>
2025-06-17 21:14:24 +0530

    Practicing primitive and non-primitive datatypes.

---
+++ LearnJavaPractice/src/main/java/org/notes/Variables.java
@@ -0,0 +1,16 @@
+package org.notes;
+
+
+/*
+    variables ->    a reusable container for a value
+                    a variable behaves as if it was the value it contains
+
+    Primitive -> simple value stored directly in memory (stack)
+    Reference -> memory address (Stack) that points to the (heap)
+
+* */
+public class Variables {
+    public static void main(String[] args){
+
+    }
+}
